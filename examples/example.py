7d2bb789780058ccc0550d90c4ecc4e0771287cf
Ignacio <52611898+ignacioph21@users.noreply.github.com>
2024-09-25 14:48:27 -0300

    Bug fixes

--- examples/example.py
+++ examples/example.py
@@ -1,11 +1,17 @@
-import sys; sys.path.insert(0, '..')
-from pyfcd.layer import Layer
-from pydata.image import *
-from pyfcd.fcd import FCD
 import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-displaced_image = Image("./example_images/sessile_drop.png", roi=-1)
-reference_image = Image("./example_images/reference.png", roi=displaced_image.roi)
+from pyfcd.layer import Layer
+from pydata.image import Image
+from pyfcd.fcd import FCD
+
+base_dir = os.path.dirname(__file__)
+displaced_image_path = os.path.join(base_dir, "example_images", "sessile_drop.png")
+reference_image_path = os.path.join(base_dir, "example_images", "reference.png")
+
+displaced_image = Image(displaced_image_path, roi=-1)
+reference_image = Image(reference_image_path, roi=displaced_image.roi)
 
 layers = [Layer(5e-3, "Air"), Layer(1e-3, "Glass"), Layer(0, "Distilled_water"), Layer(1, "Air")]
 fcd = FCD(reference_image.windowed(), layers=layers, square_size=1e-3)
